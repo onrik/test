f57b540ad034aed3d6a073ec9e7d0f2243aafd54
Andrey <and@rey.im>
2016-04-01 16:34:36 +0300

    Create runtests.py

---
+++ runtests.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+import sys
+
+import django
+from django.conf import settings
+from django.test.utils import get_runner
+
+
+if not settings.configured:
+    settings.configure(
+        DATABASES = {
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': ':memory:',
+                'USER': '',
+                'PASSWORD': '',
+                'HOST': '',
+                'PORT': '',
+            }
+        },
+
+        INSTALLED_APPS=[
+            'webshell',
+        ],
+        ROOT_URLCONF='',
+        DEBUG=False,
+    )
+
+
+def runtests():
+    django.setup()
+    TestRunner = get_runner(settings)
+    test_runner = TestRunner(verbosity=1, interactive=True)
+    
+    sys.exit(test_runner.run_tests(['webshell))
